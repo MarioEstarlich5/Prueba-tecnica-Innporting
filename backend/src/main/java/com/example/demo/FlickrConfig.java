65b1259e91f9a66f6181f8265f2e04f4051a5eab
Mario Estarlich <mario.estarlich@gmail.com>
2026-01-15 19:56:44 +0100

    add flickr class

---
+++ backend/src/main/java/com/example/demo/FlickrConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "flickr.api")
+public class FlickrConfig {
+
+    private String key;
+    private String url;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
